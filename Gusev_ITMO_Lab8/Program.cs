06da04c9f1718799c873963876a254b56841d930
999Newone999 <boris_mail@list.ru>
2022-03-11 20:14:42 +0300

    Добавьте файлы проекта.

---
+++ Gusev_ITMO_Lab8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gusev_ITMO_Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userFolder;
+            do
+            {
+                Console.Write("Введите название и путь к выбранной папке: ");
+                userFolder = Console.ReadLine();
+                if (!Directory.Exists(userFolder))
+                {
+                    Console.WriteLine("Введенной папки не существует или ее название введено неправильно.");
+                }
+            } while (!Directory.Exists(userFolder));
+
+            Console.WriteLine("Список папок в папке \"{0}\":", userFolder);
+            string[] directories = Directory.GetDirectories(userFolder);
+            foreach (string dir in directories)
+            {
+                Console.WriteLine(dir);
+            }
+
+            Console.WriteLine("Список файлов в папке \"{0}\":", userFolder);
+            string[] files = Directory.GetFiles(userFolder);
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Gusev_ITMO_Lab8_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gusev_ITMO_Lab8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Lab8_2.txt";
+            double sum=0;
+
+            Random rnd = new Random();
+
+            if (File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                for (int i = 0; i < 10; i++)
+			        {
+                        sw.WriteLine(rnd.NextDouble());
+			        }
+            }
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    sum=sum+ Convert.ToDouble(sr.ReadLine());
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
